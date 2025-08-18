4c3c6c684d8cbeb2943363760361e215504f7861
Victormaa <389796272@qq.com>
2025-08-18 11:50:22 +0800

    collectible

--- Assets/Scripts/CollectibleManager.cs
+++ Assets/Scripts/CollectibleManager.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class CollectibleManager : MonoBehaviour
 {
+    public GameObject textSystem;
+    private SpriteRenderer[] sprites;
     public static CollectibleManager Instance;
 
     public int totalCollected = 0;
@@ -14,10 +16,32 @@ public class CollectibleManager : MonoBehaviour
             Destroy(gameObject);
     }
 
+    private void Start()
+    {
+        sprites = new SpriteRenderer[textSystem.transform.childCount];
+        for (int i = 0; i < textSystem.transform.childCount; ++i)
+        {
+            var a = textSystem.transform.GetChild(i);
+            sprites[i] = a.GetComponent<SpriteRenderer>();
+        }
+    }
+
     public void Collect(int value)
     {
         totalCollected += value;
+        UpdateTextState();
+
         Debug.Log("已收集物品数：" + totalCollected);
         // 更新文字状态
+    }
+
+    private void UpdateTextState()
+    {
+        for (int i = 0; i < sprites.Length; ++i)
+        {
+            sprites[i].gameObject.SetActive(false);// .enabled = false;
+        }
+
+        sprites[totalCollected].gameObject.SetActive(true);
     }
 }
